864f62a5639b4d2ef0f4dbd877f5a06234421426
Maxime Tournier <mxm.tournier@gmail.com>
2024-05-05 17:38:02 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+
+  /******************************************************************************************************************
+   *
+   * VARIABLE GLOBAL
+   *
+   * ******************************************************************************************************************/
+
+
+  AppEnv: string = "DEV"; // DEV or PROD
+
+  /*URL API*/
+  urlTyroServApi_Dev: string = "http://localhost/Api-GetTyroServ/";
+  urlUseritiumApi_Dev: string = "http://localhost/ApiUsertium/";
+
+  urlTyroServApi_Prod: string = "http://vps207.tyrolium.fr/api/";
+  urlUseritiumApi_Prod: string = "https://useritium.fr/api-externe/";
+
+  /*FUNCTIONNEMENT*/
   isLog: boolean = true;
+  Debug:Boolean = true;
+  curentDate: Date = new Date();
+
+  /******************************************************************************************************************
+   *
+   * CONNEXION
+   *
+   * ******************************************************************************************************************/
+
+
+
+
+
+
+
+  /*****************************************************************************************************************
+   *
+   * FUNCTION GLOBAL
+   *
+   * ******************************************************************************************************************/
+
+
+
+  //SET URL API
+  setURL(type:string):string {
+    if (this.AppEnv == "DEV" && type == "useritum"){
+      return this.urlUseritiumApi_Dev
+    } else if (this.AppEnv == "DEV" && type == "tyroserv"){
+      return this.urlTyroServApi_Dev
+    } else if (this.AppEnv == "PROD" && type == "useritum") {
+      return this.urlUseritiumApi_Prod
+    } else {
+      return this.urlTyroServApi_Prod
+    }
+  }
 
 }
