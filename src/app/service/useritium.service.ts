ddfc78fdc97af321212336f0e69fd1241f557f7a
Maxime Tournier <mxm.tournier@gmail.com>
2024-05-06 15:50:10 +0200

    Update useritium.service.ts

--- src/app/service/useritium.service.ts
+++ src/app/service/useritium.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {CallUseritiumApiInterface} from "../interface/call-useritium-api.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UseritiumService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  connection(body:string,  url:string): Observable<CallUseritiumApiInterface> {
+    return this.http.post<CallUseritiumApiInterface>(url + '?controller=TyroServ&task=connectPanelAdmin', body);
+  }
+
+
 }
